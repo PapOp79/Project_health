db03ee69cd21c2999872498a2aa96fcbbb8f8415
KGITBank <KGITBank@DESKTOP-5ODFBJG>
2019-12-09 16:39:32 +0900

    wjw_1209

--- healthProjectSample/src/main/java/com/health/service/LoginCheckServiceImpl.java
+++ healthProjectSample/src/main/java/com/health/service/LoginCheckServiceImpl.java
@@ -38,6 +38,7 @@ public class LoginCheckServiceImpl implements LoginService{
 			HttpSession session = request.getSession();
 			session.setAttribute("user_id", request.getParameter("userId").trim());		
 			session.setAttribute("user_name", user.getUserName());
+			session.setAttribute("Admin", user.getAdmin());
 			return 1;
 		}
 		return 1;
